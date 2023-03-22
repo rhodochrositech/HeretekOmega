36e4f61050051d27940a132f3867541880419c1a
rhodochrositech <86504079+rhodochrositech@users.noreply.github.com>
2023-03-22 13:05:43 -0400

    This is testing
    Testing

--- tests.py
+++ tests.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 import physical as p
-import HeretekOmega as h
+import HeretekOmega as heretek
+
+
+unit = []
+for model in Model.getModelList():
+        if 'ii Vanguard' in model.getName():
+            #print(model.getStats())
+            unit.append(model)
+unit.append(unit[1])
+unit.append(unit[1])
+unit.append(unit[1])
+VanguardUnit = Unit(1,unit)
+EvilVanguardUnit = Unit(2,unit)
+print(VanguardUnit)
+print(EvilVanguardUnit)
